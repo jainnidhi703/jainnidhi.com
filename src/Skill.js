92308eeaaac5d6857debbf2d494e02db421a9f86
Nidhi Jain <jainnidhi703@gmail.com>
2017-05-09 18:37:44 +0530

    Fixing Css for skills

--- src/Skill.js
+++ src/Skill.js
@@ -3,7 +3,7 @@ import SkillImage from "./SkillImage";
 import redis from "../public/images/redis.svg";
 import react from "../public/images/react.svg";
 import nodejs from "../public/images/nodejs.svg";
-import mongodb from "../public/images/mongodb.svg";
+// import mongodb from "../public/images/mongodb.svg";
 import kafka from "../public/images/kafka.svg";
 import java from "../public/images/java.svg";
 import hbase from "../public/images/hbase.svg";

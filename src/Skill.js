fc8bede873b55cc5355a822f0818d7e057f45c0e
Nidhi Jain <jainnidhi703@gmail.com>
2017-05-10 00:44:52 +0530

    Changing skills page

--- src/Skill.js
+++ src/Skill.js
@@ -3,10 +3,10 @@ import SkillImage from "./SkillImage";
 import redis from "../public/images/redis.svg";
 import react from "../public/images/react.svg";
 import nodejs from "../public/images/nodejs.svg";
-// import mongodb from "../public/images/mongodb.svg";
+import mongo from "../public/images/mongo.svg";
 import kafka from "../public/images/kafka.svg";
 import java from "../public/images/java.svg";
-import hbase from "../public/images/hbase.svg";
+import flink from "../public/images/flink.svg";
 import hadoop from "../public/images/hadoop.svg";
 import elasticsearch from "../public/images/elasticsearch.svg";
 
@@ -18,22 +18,19 @@ class Skill extends Component {
           <header>
             <h1>Skills</h1>
           </header>
-          <section>
-            <div className="row">
-              <SkillImage image={nodejs} url="https://nodejs.org/" />
-              <SkillImage image={hadoop} url="http://hadoop.apache.org/" />
-              <SkillImage image={hbase} url="https://hbase.apache.org/" />
-              <SkillImage image={redis} url="https://redis.io/" />
-              <SkillImage image={elasticsearch} url="https://www.elastic.co/" />
-              <SkillImage
-                image={react}
-                url="https://facebook.github.io/react/"
-              />
-              <SkillImage image={java} url="https://www.java.com/" />
-              <SkillImage image={kafka} url="https://kafka.apache.org/" />
-              {/*<SkillImage image={mongodb} />*/}
+          <div className="menu">
+            <div className="col-xs-12 col-sm-12 col-lg-12">
+              <SkillImage image={java} />
+              <SkillImage image={nodejs} />
+              <SkillImage image={hadoop} />
+              <SkillImage image={flink} />
+              <SkillImage image={kafka} />
+              <SkillImage image={redis} />
+              <SkillImage image={elasticsearch} />
+              <SkillImage image={react} />
+              <SkillImage image={mongo} />
             </div>
-          </section>
+          </div>
         </div>
       </article>
     );
--- src/SkillImage.js
+++ src/SkillImage.js
@@ -3,11 +3,9 @@ import React, { Component } from "react";
 class SkillImage extends Component {
   render() {
     return (
-      <div className="3u 12u$(mobile)">
-        <a className="image fit" href={this.props.url} target="_blank">
-          <img src={this.props.image} alt="" />
-        </a>
-      </div>
+      <span className="menu-li">
+          <img src={this.props.image} alt="" className="menu-img" />
+      </span>
     );
   }
 }

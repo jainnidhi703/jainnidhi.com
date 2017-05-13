f4b8e368655c3482f6ce7ef51d718a8e78494dc8
Nidhi Jain <jainnidhi703@gmail.com>
2017-05-13 20:31:17 +0530

    Adding triangle Animation while active

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -4,23 +4,28 @@ import { NavLink, withRouter } from "react-router-dom";
 class NavigationBar extends Component {
   render() {
     const pathName = this.props.location.pathname;
-    const aboutClass = pathName === '/' ? 'active': ''; 
+    const aboutClass = pathName === "/" ? "active" : "";
     return (
       <div>
         <nav id="nav">
           <NavLink to="/about" activeClassName="active" className={aboutClass}>
+            <div className="activeTriangle" />
             <i className="icon-home" />
           </NavLink>
           <NavLink to="/work" activeClassName="active">
+            <div className="activeTriangle" />
             <i className="icon-folder" />
           </NavLink>
           <NavLink to="/projects" activeClassName="active">
+            <div className="activeTriangle" />
             <i className="icon-code" />
           </NavLink>
           <NavLink to="/technologies" activeClassName="active">
+            <div className="activeTriangle" />
             <i className="icon-lightbulb-o" />
           </NavLink>
           <NavLink to="/contact" activeClassName="active">
+            <div className="activeTriangle" />
             <i className="icon-envelope" />
           </NavLink>
         </nav>

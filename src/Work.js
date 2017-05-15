72898dcf3d1529de17f7422618f869e9fbf67e17
Nidhi Jain <jainnidhi703@gmail.com>
2017-05-15 23:56:28 +0530

    Formatting Work Page

--- src/Work.js
+++ src/Work.js
@@ -18,19 +18,25 @@ class Work extends Component {
             </div>
 
             <div className="flex-item">
-              <div>
-                <b>DailyHunt - Software Engineer</b>
-                <i style={{ float: "right" }}> [ 07/2015 - Present ]</i>
+              <div className="comicSans">
+                DailyHunt - Software Engineer
+                <i> [ 07/2015 - Present ]</i>
               </div>
               <ul>
                 <li>
-                  Being a part of Personalization & Analytics team, worked on systems to build User Preferences and Feed Recommendations for 40 million users.
+                  Built
+                  <b> Feed Recommendation Engine </b>
+                  scaled to 40 million users' affinity
                 </li>
                 <li>
-                  Developed content ingestion and meta-system for 17 languages, about 450 newspapers and  50,000 articles on day to day basis.
+                  Developed a
+                  <b> Content Ingestion System </b>
+                  that stores 50K articles/day spread across 17 languages and 450 newspapers
                 </li>
                 <li>
-                  Created dashboards and summarized email generator systems for business to see the overall impact of recommendation service.
+                  Created
+                  <b> Dashboards </b>
+                  to summarize the overall impact of the recommendation engine
                 </li>
               </ul>
             </div>
@@ -43,13 +49,16 @@ class Work extends Component {
             </div>
 
             <div className="flex-item">
-              <div>
-                <b>Juniper Networks - Intern</b>
-                <i style={{ float: "right" }}> [ 01/2015 - 06/2015 ]</i>
+              <div className="comicSans">
+                Juniper Networks - Intern
+                <i>[ 01/2015 - 06/2015 ]</i>
               </div>
               <ul>
                 <li>
-                  As a member of the security team, I built tools to extract/process Internet Session Metadata and block harmful IP addresses on top of the router.
+
+                  Built tools to
+                  <b> Extract/Process Internet Session Data </b>
+                  and block harmful IP addresses on top of the router.
                 </li>
               </ul>
             </div>

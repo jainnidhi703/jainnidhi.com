4fd13d6b883e58d431ebdc85e639facafacc9015
Nidhi Jain <jainnidhi703@gmail.com>
2017-05-24 13:22:14 +0530

    Changing Projects Content

--- src/store/ProjectStore.js
+++ src/store/ProjectStore.js
@@ -1,30 +1,36 @@
 export default [
   {
     name: "Recommendation Engine",
-    description: "Recommendation API's serves feed based on the combination of consumption and default items evaluated by a set of rules to the app. The Engine also builds user's preference aggregating views, clicks, favorites, etc.",
+    description: "Recommendation Engine serves feed based on the combination of consumption and default items evaluated by a set of rules. It also builds user's preference aggregating views, clicks, favorites, etc.",
     technologies: ["Java", "DropWizard", "HBase", "Redis"],
   },
   {
     name: "Flink Aggregations",
-    description: "Batch and Stream jobs( Lambda Architecture ) to aggregate the events from users app and build user profiles, extract out all necessary information such as installs, clicks, etc.",
+    description: "Batch and Stream jobs(Lambda Architecture) to aggregate the app events, build user profile and extract out necessary information such as installs, clicks, etc",
     technologies: ["Java", "Flink", "Kafka", "MongoDB", "Hadoop"]
   },
   {
     name: "Content Ingestion System",
-    description: "Multilingual content is tagged and enriched with all the metadata such as newspapers, language, publisher, similar items, etc.",
+    description: "Multilingual content is tagged and enriched with metadata such as newspapers, language, publisher, similar items, etc",
     technologies: ["Java", "NodeJS", "Elastic", "DropWizard", "Kafka"]
+  },
+  {
+    name: "jainnidhi.com",
+    gitUrl: "https://github.com/jainnidhi703/jainnidhi.com",
+    description: "Single Page Personal Website with animations",
+    technologies: ["React", "CSS"]
   },
   {
     name: "History Reconstruction",
     gitUrl: "https://github.com/jainnidhi703/HistoryReconstruction",
-    description: "Summarize News documents in a chronologically sorted order using Lucene to find similarity scores.",
+    description: "Summarize News documents in a chronologically sorted order using Lucene to find similarity scores",
     technologies: ["Java", "Lucene"]
   },
   {
     name: "Open Source Contribution",
     projectUrl: "https://okular.kde.org/",
     projectUrlText: "Okular KDE",
-    description: "Worked on some issues related to audio support in epub for Okular KDE.",
+    description: "Worked on issues related to audio support in epub for Okular KDE",
     technologies: ["Qt", "C++"]
   }
 ];

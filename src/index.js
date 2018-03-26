e74c9f14a31ab2ec91120830bd0e2377a59f561e
Nidhi Jain <nidhi.j@meawwworld.com>
2018-03-26 22:33:11 +0530

    adding google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-116415115-1');
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(
   <BrowserRouter>
